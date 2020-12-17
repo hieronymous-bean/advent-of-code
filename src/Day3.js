0ce6c249dd5b7d950388ad68ce0d065b2dc020e0
hieronymous-bean <jeffrey@jeffreyallanbrown.com>
2020-12-16 21:15:23 -0500

    Map Matrix Functionality

--- src/Day3.js
+++ src/Day3.js
@@ -4,7 +4,7 @@ require.extensions['.txt'] = function (module, filename) {
 };
 
 const data = require("../data/Day3.txt");
-const dataRow = data.split('\n');
+const dataRows = data.split('\n');
 
 const movement = [[3, -1]];
 
@@ -13,14 +13,44 @@ function treeCounter(data, movement) {
 };
 
 
-// Process Tree Map //
+// Process Map //
 function toArray(data) {
-	var dataRow = data.split('\n');
 	var dataArray = [];
 
 	for (i = 0; i < dataRow.length; i++) {
     	
 
     }
-    
+
 };
+
+
+function mapToMatrix(mapRowsArray){
+	var rows = 0;
+	var columns = 0;
+	var map = [];
+
+	for (ia = 0; ia < mapRowsArray.length; ia++) {
+		let currentRow = mapRowsArray[ia];
+		
+		for (ib = 0; ib < currentRow.length; ib++) {
+
+			if (currentRow[ib] == '#') {
+
+				ic = 1;
+
+			}
+
+			else {
+
+				ic = 0;
+			}
+
+			map.push([ia, ib, ic]);
+		}
+
+	};
+	return map;
+};
+
+mapToMatrix(dataRows);
