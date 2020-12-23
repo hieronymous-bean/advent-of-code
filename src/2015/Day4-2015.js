1097b2799b92fd91a6a5501799fdcbf543e89dd7
hieronymous-bean <jeffrey@jeffreyallanbrown.com>
2020-12-23 15:38:41 -0500

    MD5 Troubleshooting for Day4

---
+++ src/2015/Day4-2015.js
@@ -0,0 +1,18 @@
+var md5 = require('md5');
+
+// Step 1. Prep Input //
+function AppendBits(input) {
+	let totalBits = (input.length * 8);
+};
+//
+
+// Step 2.
+
+
+// Step 3.
+
+
+// Step 4.
+
+
+console.log(md5('atesting'));
